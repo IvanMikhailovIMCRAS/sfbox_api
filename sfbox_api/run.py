db736b486459951fa2710bb9065927e1d9cfca53
Ivan Mikhailov <mikhailov.ivan.imc@gmail.com>
2023-10-30 11:38:16 +0300

    experimental dist 2

--- sfbox_api/run.py
+++ sfbox_api/run.py
@@ -1,8 +1,10 @@
 import os
 
 def run(word: str) -> int:
-    with open(os.path.join(os.getcwd(),"data/test.in"), 'w') as f:
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    parent_directory = os.path.split(current_directory)[0]
+    with open(os.path.join(parent_directory,"data/test.in"), 'w') as f:
         f.write(word)
-    with open("./data/test.in", 'r') as f:
+    with open(os.path.join(parent_directory,"data/test.in"), 'r') as f:
         s = f.read()
     return len(s)
