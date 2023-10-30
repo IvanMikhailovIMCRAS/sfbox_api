84b14b460fe251217abcc19bcaca8153ed5432b5
Ivan Mikhailov <mikhailov.ivan.imc@gmail.com>
2023-10-30 11:18:40 +0300

    experimental dist

--- sfbox_api/run.py
+++ sfbox_api/run.py
@@ -1,6 +1,7 @@
+import os
 
 def run(word: str) -> int:
-    with open("./data/test.in", 'w') as f:
+    with open(os.path.join(os.getcwd(),"data/test.in"), 'w') as f:
         f.write(word)
     with open("./data/test.in", 'r') as f:
         s = f.read()
