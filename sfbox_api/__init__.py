d432a8f9ac360115366746a9f26ace376d103826
Ivan Mikhailov <mikhailov.ivan.imc@gmail.com>
2023-11-02 10:04:06 +0300

    add sfbox and Lat

--- sfbox_api/__init__.py
+++ sfbox_api/__init__.py
@@ -1,4 +1,10 @@
+import os
+
 from .composition import Composition
 from .run import run
+
+target = f"{os.path.dirname(os.path.realpath(__file__))}/data/sfbox"
+os.system(f"chmod +x {target}")
+
 
 __all__ = ["Composition", "run"]
