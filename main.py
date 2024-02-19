01c38a7742255ec753a83678312c4770380e5492
Ivan Mikhailov <mikhailov.ivan.imc@gmail.com>
2024-02-19 10:28:56 +0300

    rewriting barbwire

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from sfbox_api import polyacid
+from sfbox_api import polyacid, barbwire
 
 if __name__ == "__main__":
     frame = polyacid(N=100, sigma=0.16, pK=1.0, ionic_strength=1e-3, chi=0.5)
     print(frame.profile_labels)
-    # print(frame.profile["layer"])
+
+    bw = barbwire(p=3, n=100, m=100, q=2, root=2, theta=5, chi=0, n_layers=200)
+    print(bw.profile_labels)
+
--- sfbox_api/zoo.py
+++ sfbox_api/zoo.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from .frame import Frame
 from .lattice import Lat
 from .molecule import Mol
@@ -47,37 +48,54 @@ def comb_brush(Nb: int, n: int, m: int, n_layers, sigma: float) -> Frame:
     return frame
 
 
-def barbwire(p, n, m, q, n_layers, sigma):
-    comp = f"(X)1(A){m-1}([(A){n}]){q}((A){m}([(A){n}]){q}){p-1}(A){m-1}(G)1"
-    N = (m + q * n) * p + m
-    theta = N * sigma
-    lat = Lat.model_validate({"n_layers": n_layers, "geometry": "cylindrical"})
+def barbwire(p: int, n: int, m: int, q: int, root: int, 
+             theta: float, chi: float, n_layers: int, 
+             geometry: Literal['flat','cylindrical','spherical']='flat') -> Frame:
+    # validating parameters
+    if p < 1: raise ValueError("p < 1")
+    if n < 1: raise ValueError("n < 1")
+    if m < 2: raise ValueError("m < 2")
+    if q < 1: raise ValueError("q < 1")
+    if root < 1: raise ValueError("root < 1")
+    if n_layers < int(theta)+1: raise ValueError("n_layer < theta+1")
+    # setting composition 
+    if root == 1:
+        root_block = f"(X)1"
+    else:
+        root_block = f"(X)1(A){root-1}"
+    
+    if q == 1:
+        side_block = f"[(A){n}]"
+    else:
+        side_block = f"([(A){n}]){q}"
+
+    if p == 1:
+        comp = f"{root_block}{side_block}(A){m-1}(G)1"
+    else:
+        comp = f"{root_block}{side_block}((A){m}{side_block}){p-1}(A){m-1}(G)1"
+
+    # lattice parameters
+    lat = Lat.model_validate({"n_layers": n_layers, "geometry": geometry})
     mons = [
         Mon.model_validate({"name": "X", "freedom": "pinned", "pinned_range": "1"}),
         Mon.model_validate({"name": "A", "freedom": "free"}),
         Mon.model_validate({"name": "G", "freedom": "free"}),
         Mon.model_validate({"name": "W", "freedom": "free"}),
     ]
+    # set of molecules
     mols = [
         Mol.model_validate(
             {"name": "Water", "composition": "(W)1", "freedom": "solvent"}
         ),
         Mol.model_validate(
-            {
-                "name": "pol",
-                "composition": comp,
-                "freedom": "restricted",
-                "theta": theta,
-            }
-        ),
+            {"name": "pol", "composition": comp, "freedom": "restricted", "theta": theta}
+        )
     ]
-    sys = Sys()
-    chi = 0.0
-    chi_list = {"X W": chi, "A W": chi}
-
-    frame = Frame(lat, sys, mols, mons, chi_list=chi_list)
+    # Flory-Hugggins interactions
+    chi_list = {"X W": chi, "A W": chi, "G W": chi}
+    # collecting frame for calculation
+    frame = Frame(lat, Sys(), mols, mons, chi_list=chi_list)
     frame.run()
-
     return frame
 
 
